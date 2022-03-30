df8bbfd4ed4cda35cebbd9a2ccd5f3b8a9c3f59b
Aaron <aaronfarmer89@yahoo.com>
2022-03-29 22:05:07 -0500

    Used the IconButton

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -4,6 +4,8 @@ import { Link } from "gatsby"
 import styled from "styled-components"
 import { H1 } from "../Heading"
 import { Section } from '../Section'
+import Search from 'styled-icons/feather'
+import { IconButton } from '../Button'
 
 const Outer = styled.header`
   background: ${props => props.theme.header.backgroundColor};
@@ -35,7 +37,7 @@ const Header = ({ siteTitle }) => (
           </H1>
         </Section>
         <Section width={1/12}>
-        Search
+        <IconButton icon={<Search/>}/>
         </Section>
       </Section>
     </Inner>
